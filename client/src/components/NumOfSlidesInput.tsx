3c5ba524ca8c81dc3d467a563509a0350c86f10b
Zion Pulapakura <kingofbeastz777@gmail.com>
2025-11-10 00:16:56 +1100

    num of slide input disabled when generating story

--- client/src/components/NumOfSlidesInput.tsx
+++ client/src/components/NumOfSlidesInput.tsx
@@ -1,14 +1,17 @@
 import { TextField } from "@mui/material";
 import useStoryStore from "../store/storyStore";
+import useStoryInputStore from "../store/storyInputStore";
 
 const NumOfSlidesInput = () => {
   const { numOfSlides, setNumOfSlides } = useStoryStore();
+  const { isGenerating } = useStoryInputStore();
 
   return (
     <div>
       <h3>Number of slides to generate</h3>
 
       <TextField
+        disabled={isGenerating}
         onChange={(e) => {
           const val = e.target.value;
           const num = Number(val);
